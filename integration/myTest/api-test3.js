ffa9e9c7c8415c8ec975668109c3e8713073ab93
Charles Davies <59093792+charlesDavies@users.noreply.github.com>
2022-02-18 23:42:24 +0100

    Add files via upload

--- integration/myTest/api-test3.js
+++ integration/myTest/api-test3.js
@@ -7,13 +7,42 @@ describe("Backend test", ()=> {
             
         }).should((res) =>{
             expect(res.status).equal(200)
-            expect(res.body.EnableCarousel).eq("true")
-            cy.log(res.body.showText)
+            // should be log false
+            cy.log(res.body.config.settings.carousel.showBetType)
+            expect(res.body.config.settings.carousel.showBetType).eq(false)
+            // should also log false
+            cy.log(res.body.config.settings.carousel.showEventType)
+            expect(res.body.config.settings.carousel.showEventType).eq(false)
 
-            // expect(res.body).to.contain("blockedJurisdictionPerCountry")
-            // expect(res.body).to.contain("sortCountriesByList") 
-            // expect(res.body).to.contain("spashPage")
-            // expect(res.body).to.contain("enableDesktop")
+            // check blockedJurisdictionPerCountry exists
+            expect(res.body.blockedJurisdictionPerCountry).exist
+
+            // check sortCountriesByList
+            expect(res.body.config.settings.lobby.sortCountriesByList).exist
+
+            // check the splash page key exists
+            expect(res.body.config.settings.splashPage).exist
+
+            // check the enable desktop key exists
+            expect(res.body.uiConfiguration.MarketingWidget.enableDesktop).exist
+
+            // check that carouselLimit is greater than 3
+            // There must be a better way to complish this
+            expect(res.body.config.settings.carousel.carouselLimit).above(3, 4, '4 is greater than 3')
+
+            // Check twitter user key
+            expect(res.body.config.settings.app.twitterUser).equal("UnibetRacing")
+
+            //check splash page background
+            expect(res.body.config.settings.splashPage.background).eq("https://content.unibet.co.uk/p/bg-image.jpg")
+
+            //check showTimeToJumpOnMobile
+            expect(res.body.config.settings.lobby.showTimeToJumpOnMobile).equal(true)
+            //expect(res.body.config.settings.lobby.showTimeToJumpOnMobile).isNotNull(true, "is not null")
+
+            
+
+            
 
             
             
